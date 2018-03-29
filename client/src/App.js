def9362d21fc1421b66c98560f4a79c6b43e20ba
unknown <ewendel88@gmail.com>
2018-03-29 10:01:58 -0500

    Wont deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 // Modules
